dbff9ca81466660b3889ee43652fba08b3621fc5
Michael Jones <michaelj@kainos.com>
2020-02-03 09:40:07 +0000

    added redux folder and reducers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import rootReducer from './redux/reducers'
 import {BrowserRouter} from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 
+const store = createStore(rootReducer)
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
